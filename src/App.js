13708c65c74c8b0974d3a023b1121e042185d503
Santiago Hincapie <santiagohr81@hotmail.com>
2021-02-08 20:30:25 -0500

    starter code

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Portfolio from "./components/portfolio";
+import Nav from "./components/nav";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Nav /> */}
+      <Portfolio />
+    </div>
+  );
+}
+
+export default App;
